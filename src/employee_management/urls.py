7e7e1ba783b8cc66877089781ad5613b200f7003
Luis Quinones <lpaandres2020@gmail.com>
2021-11-13 18:11:32 -0500

    New and ready: crud/urls.py, employee_management/urls.py.

--- src/employee_management/urls.py
+++ src/employee_management/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import redirect
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('crud/', include('crud.urls')),
+    path('', lambda request: redirect('crud/')),
 ]

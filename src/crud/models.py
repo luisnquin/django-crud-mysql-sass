d786cd47fa13d3a78b85a9827adeb06dc02209f8
Luis Quinones <lpaandres2020@gmail.com>
2021-11-13 18:04:11 -0500

    Ready: Employee models, Migrations, /admin.py

--- src/crud/models.py
+++ src/crud/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=30, verbose_name="Firstname")
+    last_name = models.CharField(max_length=30, verbose_name="Lastname")
+    email = models.EmailField(verbose_name="Email")
+    phone = models.CharField(max_length=10, verbose_name="Phone number")
+    location = models.CharField(max_length=50, verbose_name="Location")
+    university = models.CharField(max_length=50, verbose_name="University")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} | {self.phone}"

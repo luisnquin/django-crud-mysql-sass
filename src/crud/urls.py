5c369e94be556fa16b6382bf17be2ba1caada73f
Luis Quinones <lpaandres2020@gmail.com>
2021-11-14 12:10:18 -0500

    Mod: index.html(urls, modified), new views and url's.

--- src/crud/urls.py
+++ src/crud/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, addEmployee, updateEmployee, deleteEmployee
 
 
 app_name = 'crud'
 urlpatterns = [
     path('', index),
+    path('add/', addEmployee, name="add-e"),
+    path('update/<int:id>/', updateEmployee, name="update-e"),
+    path('delete/<int:id>/', deleteEmployee, name="delete-e"),
 ]
--- src/crud/views.py
+++ src/crud/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Employee
 # Create your views here.
@@ -7,3 +8,15 @@ from .models import Employee
 def index(request):
     alldata = Employee.objects.all()
     return render(request, "index.html", {"alldata": alldata})
+
+
+def addEmployee(request):
+    return HttpResponse("Estás tratando de añadir un empleado, huh")
+
+
+def updateEmployee(request, id):
+    return HttpResponse(f"Update, your ID is {id}")
+
+
+def deleteEmployee(request, id):
+    return HttpResponse(f"Delete, your ID is {id}")
